5c97a4d94c922a02ede436d5e01785e10c6b7668
aznur <zceenur@ucl.ac.uk>
2022-02-08 12:53:01 +0000

    Added other journey types for the visualisation

--- dash_app.py
+++ dash_app.py
@@ -17,14 +17,25 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # Create a dataframe from existing CSV file with the prepared dataset
 df = pd.read_csv('prepared_dataset.csv')
 
-fig = px.line(df, x="Period ending", y="Bus journeys (m)", markers=True,
-              title="Usage of public transport by journey types in London")
+journey_types = df.iloc[:, 5:]
+
+types = journey_types.columns
+
+fig = px.line(df, x="Period ending", y=df.columns[5:], markers=True,
+              title="Usage of public transport by journey types in London", width=1200, height=500,
+              labels={"Period ending": "Period ending (date)", "value": "Amount of journeys (millions)",
+                      "variable": "Journey types:"})
 
 app.layout = html.Div(children=[
-    html.H1(children='Coursework 1'),
+    html.H1(
+        children='Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic'),
 
     html.Div(children='''
-        Dataframe with the amount of journeys for different types of public transportation in London:
+        There are many types of public transportation in London. Trends in the usage of public transport change due to 
+        the certain events such as road closures, construction works and so on. In 2020 and 2021, there were a lot of 
+        disruptions of Transport for London (TfL) because of coronavirus pandemic. New regulations and changes led to 
+        the transition of popularity for public transport journeys by type of transport. Due to an increased demand for 
+        transport, there is now a problem of overcrowding, which is a safety risk.
     '''),
 
     dcc.Graph(
