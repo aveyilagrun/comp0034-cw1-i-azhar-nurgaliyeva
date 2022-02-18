e418dbe69cda283fbe88940b122aba0a939c97aa
aznur <zceenur@ucl.ac.uk>
2022-02-18 17:33:40 +0600

    Added interactive dropdown for the journey types + simple nav.bar

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # COMP0034: Coursework 1 repository
 
-For each chart, write a markdown file (e.g. comp0034_cw1.md or in the README.md) text to:
-1.	State the target audience and the question that the visualisation is intended to address.
-2.	Explain your design for each visualisation. You can do this before you code the visualisation. 
-3.	Optionally, a stronger response to this aspect will also evaluate the resulting visualisation with respect to its design. You will only be able to do this once you have created the visualisation in your dashboard.
-There is no word limit for the explanation. As a guide, 200 words per visualisation is likely to be sufficient to explain the design.
+**[Repository URL](https://github.com/ucl-comp0035/comp0034-cw1-i-azhar-nurgaliyeva)**
 
-You do not need to provide a sketch or representation of the design, you only need to explain how you designed the visualisation that you will subsequently code in your app. For each visualisation that you design consider:
-•	The target audience for this visualisation. Consider using the relevant persona (or other format) from COMP0035.
-•	The question that this visualisation is intended to answer. Use the questions you identified in COMP0035, or you can write new questions.
-•	The data needed from your data set to answer the question
-•	The type of chart (e.g. pie, bar etc) and its appropriateness for the question/audience. Use the resources given in the teaching materials.
-•	The visual aspects of the design (e.g. style, colours, titles). Use the literature and resources introduced in the teaching materials; or other relevant sources from your independent literature research.
-[Optional] Evaluate each visualisation. You will only be able to do this step once you have coded the application and can evaluate the chart.
-1.	How well does the visualisation you created meet with the design choices you made?
-2.	What are the strengths and weaknesses of the design with respect to the target audience and the question you were intending to answer?
-3.	If relevant, suggest any ways in which your visualisation design could be improved.
+## Target audience
+
+For all the visualisations I am using the same target audience as in
+the [COMP0035: Coursework 1](https://github.com/ucl-comp0035/coursework-1-azhar-nurgaliyeva):
+
+![persona_target](persona.png)
+
+## Questions to be answered using the dataset
+
+1) Is there any pattern in the changes of usage for public transportation in London?
+2) Which group of public transportation consumers use the most?
+3) Which category of people usually uses this type of transport the most?
+4) Are trends similar to the performance before the pandemic?
+5) Is it possible to reduce the overcrowding on certain types of transport?
+
+## Explanation and evaluation of the chosen visualisations
+
+1. Line graph
+2. Bar chart
+3. Pie chart
+
+## References
--- dash_app.py
+++ dash_app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import plotly.express as px
 from dash import dcc
 from dash import html
+from dash import Input, Output
 
 external_stylesheets = [dbc.themes.PULSE]
 
@@ -21,30 +22,73 @@ journey_types = df.iloc[:, 5:]
 
 types = journey_types.columns
 
-fig = px.line(df, x="Period ending", y=df.columns[5:], markers=True,
-              title="Usage of public transport by journey types in London", width=1200, height=500,
-              labels={"Period ending": "Period ending (date)", "value": "Amount of journeys (millions)",
-                      "variable": "Journey types:"})
+line_fig = px.line(df, x="Period ending", y=df.columns[5:], markers=True,
+                   title="Usage of public transport by journey types in London", width=1200, height=500,
+                   labels={"Period ending": "Period ending (date)", "value": "Amount of journeys (millions)",
+                           "variable": "Journey types:"})
 
 app.layout = html.Div(children=[
+
+    html.Div(children=[dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+            dbc.DropdownMenu(
+                children=[
+                    dbc.DropdownMenuItem("More pages", header=True),
+                    dbc.DropdownMenuItem("Page 2", href="#"),
+                    dbc.DropdownMenuItem("Page 3", href="#"),
+                ],
+                nav=True,
+                in_navbar=True,
+                label="More",
+            ),
+        ],
+        brand="NavbarSimple",
+        brand_href="#",
+        color="primary",
+        dark=True,
+    )]),
+
     html.H1(
         children='Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic'),
 
     html.Div(children='''
-        There are many types of public transportation in London. Trends in the usage of public transport change due to 
-        the certain events such as road closures, construction works and so on. In 2020 and 2021, there were a lot of 
-        disruptions of Transport for London (TfL) because of coronavirus pandemic. New regulations and changes led to 
-        the transition of popularity for public transport journeys by type of transport. Due to an increased demand for 
+        There are many types of public transportation in London. Trends in the usage of public transport change due to
+        the certain events such as road closures, construction works and so on. In 2020 and 2021, there were a lot of
+        disruptions of Transport for London (TfL) because of coronavirus pandemic. New regulations and changes led to
+        the transition of popularity for public transport journeys by type of transport. Due to an increased demand for
         transport, there is now a problem of overcrowding, which is a safety risk.
     '''),
 
+    html.Div(
+        [dcc.Dropdown(
+            id="journey-types-dropdown",
+            options=[{
+                'label': i,
+                'value': i
+            } for i in types],
+            value=types),
+        ],
+        style={'width': '30%',
+               'display': 'inline-block'}),
+
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='line-graph',
+        figure=line_fig
     ),
 
     html.Button(children="Button", className="btn btn-primary")
 ])
+
+@app.callback(
+    Output(component_id='line-graph', component_property='figure'),
+    Input(component_id='journey-types-dropdown', component_property='value')
+)
+def update_graph(selected_type):
+    filtered_journeys = df[['Period ending', f'{selected_type}']]
+    line_fig = px.line(filtered_journeys, x='Period ending', y=f'{selected_type}',
+                       title=f'Usage of {selected_type} in London')
+    return line_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
