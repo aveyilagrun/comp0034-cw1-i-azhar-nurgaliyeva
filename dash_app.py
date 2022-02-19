eda0ebe5449f13ac1334702e897459918f987c80
aznur <zceenur@ucl.ac.uk>
2022-02-19 23:44:10 +0600

    Added second visualisation for cumulative usage of transport

--- dash_app.py
+++ dash_app.py
@@ -1,8 +1,3 @@
-# Copied from the Dash tutorial documentation at https://dash.plotly.com/layout on 24/05/2021
-# Import section modified 10/10/2021 to comply with changes in the Dash library.
-
-# Run this app with `python dash_app.py` and visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -12,14 +7,12 @@ from dash import html
 from dash import Input, Output
 
 external_stylesheets = [dbc.themes.PULSE]
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Create a dataframe from existing CSV file with the prepared dataset
 df = pd.read_csv('prepared_dataset.csv')
 
 journey_types = df.iloc[:, 5:]
-
 types = journey_types.columns
 
 line_fig = px.line(df, x="Period ending", y=df.columns[5:], markers=True,
@@ -27,68 +20,68 @@ line_fig = px.line(df, x="Period ending", y=df.columns[5:], markers=True,
                    labels={"Period ending": "Period ending (date)", "value": "Amount of journeys (millions)",
                            "variable": "Journey types:"})
 
+bar_fig = px.bar(df, x="Period ending", y=df.columns[5:],
+                 title="Cumulative usage of public transport by journey types in London", width=1200, height=500,
+                 labels={"index": "Period ending (date)", "value": "Amount of journeys (millions)",
+                         "variable": "Journey types:"})
+
+line_graph_tab = html.Div(children=[])
+
 app.layout = html.Div(children=[
 
     html.Div(children=[dbc.NavbarSimple(
         children=[
-            dbc.NavItem(dbc.NavLink("Page 1", href="#")),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem("More pages", header=True),
-                    dbc.DropdownMenuItem("Page 2", href="#"),
-                    dbc.DropdownMenuItem("Page 3", href="#"),
-                ],
-                nav=True,
-                in_navbar=True,
-                label="More",
-            ),
+            dbc.NavItem(dbc.NavLink(
+                "Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic",
+                href="#"))
         ],
-        brand="NavbarSimple",
+        brand="COMP0034: Coursework 1",
         brand_href="#",
         color="primary",
         dark=True,
     )]),
 
-    html.H1(
-        children='Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic'),
-
-    html.Div(children='''
-        There are many types of public transportation in London. Trends in the usage of public transport change due to
-        the certain events such as road closures, construction works and so on. In 2020 and 2021, there were a lot of
-        disruptions of Transport for London (TfL) because of coronavirus pandemic. New regulations and changes led to
-        the transition of popularity for public transport journeys by type of transport. Due to an increased demand for
-        transport, there is now a problem of overcrowding, which is a safety risk.
-    '''),
+    html.Div([html.H4('Description of the data'),
+              html.Div('''There are many types of public transportation in London. Trends in the usage of 
+                       public transport change due to the certain events such as road closures, construction works 
+                       and so on. In 2020 and 2021, there were a lot of disruptions of Transport for London (TfL) 
+                       because of coronavirus pandemic. New regulations and changes led to the transition of popularity 
+                       for public transport journeys by type of transport. Due to an increased demand for transport, 
+                       there is now a problem of overcrowding, which is a safety risk.''')]),
 
     html.Div(
-        [dcc.Dropdown(
+        [html.H4('Choose a journey type from the dropdown list:'),
+        dcc.Dropdown(
             id="journey-types-dropdown",
             options=[{
                 'label': i,
                 'value': i
-            } for i in types],
-            value=types),
+            } for i in types], placeholder='Select a journey type...'),
         ],
         style={'width': '30%',
-               'display': 'inline-block'}),
+               'display': 'inline'}),
 
     dcc.Graph(
         id='line-graph',
         figure=line_fig
     ),
 
-    html.Button(children="Button", className="btn btn-primary")
+    dcc.Graph(
+        id='bar-graph',
+        figure=bar_fig
+    )
 ])
 
+# Callback for line graph
 @app.callback(
     Output(component_id='line-graph', component_property='figure'),
-    Input(component_id='journey-types-dropdown', component_property='value')
-)
+    Input(component_id='journey-types-dropdown', component_property='value'))
 def update_graph(selected_type):
     filtered_journeys = df[['Period ending', f'{selected_type}']]
     line_fig = px.line(filtered_journeys, x='Period ending', y=f'{selected_type}',
                        title=f'Usage of {selected_type} in London')
     return line_fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
