8f42892efa6dd22c7a91bda457b7b850e3bb2667
aznur <zceenur@ucl.ac.uk>
2022-02-27 16:33:39 +0000

    2 more visualisations added + using multiple tabs now

--- dash_app.py
+++ dash_app.py
@@ -3,10 +3,11 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 from dash import dcc, html, Input, Output
-from datetime import datetime
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 external_stylesheets = [dbc.themes.PULSE]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 # Create a dataframe from existing CSV file with the prepared dataset
 df = pd.read_csv('prepared_dataset.csv')
@@ -32,64 +33,57 @@ pie_df = df[
 
 pie_fig = px.pie(values=pie_df.values, names=pie_df.index, title="Amount of journeys in a year")
 
-# Graph to compare same periods from different financial years?
+year_fig = make_subplots(rows=2, cols=2,
+                         subplot_titles=("Overground journeys", "DLR journeys", "Tram journeys", "TfL Rail journeys"))
 
-app.layout = html.Div(children=[
+year_fig.add_trace(go.Scatter(x=df['Period ending'], y=df['Overground journeys (m)'] * 1000000), row=1, col=1)
 
-    html.Div(children=[dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink(
-                "Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic",
-                href="#"))
-        ],
-        brand="COMP0034: Coursework 1",
-        brand_href="#",
-        color="primary",
-        dark=True,
-    )]),
+year_fig.add_trace(go.Scatter(x=df['Period ending'], y=df['DLR journeys (m)'] * 1000000), row=1, col=2)
 
-    html.Div([html.H4('Description of the data'),
-              html.Div('''There are many types of public transportation in London. Trends in the usage of 
-                       public transport change due to the certain events such as road closures, construction works 
-                       and so on. In 2020 and 2021, there were a lot of disruptions of Transport for London (TfL) 
-                       because of coronavirus pandemic. New regulations and changes led to the transition of popularity 
-                       for public transport journeys by type of transport. Due to an increased demand for transport, 
-                       there is now a problem of overcrowding, which is a safety risk.''')],
-             style={'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px"}),
+year_fig.add_trace(go.Scatter(x=df['Period ending'], y=df['Tram journeys (m)'] * 1000000), row=2, col=1)
 
-    html.Div(html.H4('Choose a journey type from the dropdown list:'),
-             style={'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px"}),
+year_fig.add_trace(go.Scatter(x=df['Period ending'], y=df['TfL Rail journeys (m)'] * 1000000), row=2, col=2)
 
-    html.Div(
-        dcc.Dropdown(
-            id="journey-types-dropdown",
-            options=[{
-                'label': i,
-                'value': i
-            } for i in types], placeholder='Select a journey type...'),
-        style={'width': '40%',
-               'display': 'inline-block', 'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px",
-               'textAlign': 'center'}),
+year_fig.update_layout(height=500, width=1200,
+                       title_text="Usage of overground, DLR, tram and TfL rail journey types in London")
 
-    html.Div(dcc.Graph(
+box_fig = px.box(df, y=df.columns[5:])
+
+line_graph_tab = html.Div([
+    html.H4('Choose a journey type from the dropdown list:'),
+    dcc.Dropdown(
+        id="journey-types-dropdown",
+        options=[{
+            'label': i,
+            'value': i
+        } for i in types], placeholder='Select a journey type...'),
+
+    dcc.Graph(
         id='line-graph',
         figure=line_fig
-    ), style={'margin-left': '30px', 'margin-right': "30px", 'margin-top': "30px"}),
+    ),
+],
+    style={'width': '40%', 'display': 'inline-block', 'margin-left': '80px',
+           'margin-right': "25px", 'margin-top': "30px", 'textAlign': 'center'})
 
-    html.Div(dcc.Graph(
+bar_graph_tab = html.Div([
+    dcc.Graph(
         id='bar-graph',
         figure=bar_fig
-    ), style={'margin-left': '30px', 'margin-right': "30px", 'margin-top': "30px"}),
+    )
+],
+    style={'width': '40%', 'display': 'inline-block', 'margin-left': '80px',
+           'margin-right': "25px", 'margin-top': "30px", 'textAlign': 'center'})
 
-    html.Div(dcc.Graph(
+pie_chart_tab = html.Div([
+    dcc.Graph(
         id='pie-graph',
         figure=pie_fig
-    ), style={'margin-left': '30px', 'margin-right': "30px", 'margin-top': "30px"}),
+    ),
 
-    html.Div(html.H4('Choose a timeline to compare popularity in journey types:'),
-             style={'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px"}),
+    html.H4('Choose a timeline to compare popularity in journey types:'),
 
-    html.Div(dcc.RangeSlider(
+    dcc.RangeSlider(
         18,
         21,
         step=1,
@@ -100,9 +94,83 @@ app.layout = html.Div(children=[
             19: '2019',
             20: '2020',
             21: '2021'}
-    ), style={'width': '50%', 'display': 'inline-block', 'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px"})
+    ),
+],
+    style={'width': '40%', 'display': 'inline-block', 'margin-left': '80px',
+           'margin-right': "25px", 'margin-top': "30px", 'textAlign': 'center'})
+
+subplots_tab = html.Div([
+    dcc.Graph(
+        id='time-graph',
+        figure=year_fig
+    )
+], style={'margin-left': '30px', 'margin-right': "30px", 'margin-top': "30px"})
+
+box_plots_tab = html.Div([
+    html.H4('Choose journey types to compare box plots:'),
+
+    dcc.RadioItems(
+        id='y-axis',
+        options=[{'value': x, 'label': x}
+                 for x in df.columns[5:]],
+        value='Bus journeys (m)',
+        labelStyle={'display': 'inline-block', 'margin-left': '80px', 'margin-right': "25px",
+                    'margin-top': "30px"}
+    ),
+
+    dcc.Graph(
+        id='box-graph',
+        figure=box_fig
+    ),
+], style={'margin-left': '30px', 'margin-right': "30px", 'margin-top': "30px"})
+
+app.layout = html.Div([
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink(
+                "Exploratory analysis on how public transport use in London has changed due to the coronavirus pandemic",
+                href="#"))
+        ],
+        brand="COMP0034: Coursework 1",
+        brand_href="#",
+        color="primary",
+        dark=True,
+    ),
+
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Line graph', value='tab-1'),
+        dcc.Tab(label='Bar chart', value='tab-2'),
+        dcc.Tab(label='Pie chart', value='tab-3'),
+        dcc.Tab(label='Subplots', value='tab-4'),
+        dcc.Tab(label='Box plots', value='tab-5')],
+             style={'textAlign': 'center',
+                    'border': '2px solid blue'}), html.Div(id='tabs-content'),
+
+    html.Div([html.H4('Description of the data'),
+              html.Div('''There are many types of public transportation in London. Trends in the usage of
+                       public transport change due to the certain events such as road closures, construction works
+                       and so on. In 2020 and 2021, there were a lot of disruptions of Transport for London (TfL)
+                       because of coronavirus pandemic. New regulations and changes led to the transition of popularity
+                       for public transport journeys by type of transport. Due to an increased demand for transport,
+                       there is now a problem of overcrowding, which is a safety risk.''')],
+             style={'margin-left': '80px', 'margin-right': "25px", 'margin-top': "30px"}),
 
 ])
+
+
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'tab-1':
+        return line_graph_tab
+    elif tab == 'tab-2':
+        return bar_graph_tab
+    elif tab == 'tab-3':
+        return pie_chart_tab
+    elif tab == 'tab-4':
+        return subplots_tab
+    elif tab == 'tab-5':
+        return box_plots_tab
 
 
 # Callback for line graph
@@ -149,6 +217,15 @@ def update_pie_graph(selected_year):
     pie_fig_new = px.pie(values=pie_df_new.values, names=pie_df_new.index,
                          title="Amount of journeys in a year")
     return pie_fig_new
+
+
+# Callback for box plots
+@app.callback(
+    Output("box-graph", "figure"),
+    Input("y-axis", "value"))
+def generate_chart(y):
+    box_fig = px.box(df, y=df[y])
+    return box_fig
 
 
 if __name__ == '__main__':
